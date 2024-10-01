ce0ccdc9138c288d508b35e9144fc9efaab3e115
Deepakyadav9966 <deepakchenikala@gmail.com>
2024-10-01 07:48:23 +0000

    first commit

---
+++ src/components/UserInfo/index.js
@@ -0,0 +1,16 @@
+// Write your JS code here
+import './index.css'
+
+const UserInfo = () => (
+  <>
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/profile-img.png"
+      alt="profile"
+      className="profile-image"
+    />
+    <h3>Wade Warren</h3>
+    <p>Software developer at UK</p>
+  </>
+)
+
+export default UserInfo
